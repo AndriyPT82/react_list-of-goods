3821a1e79cd0f97332aa5f3cc863bec724bc6a3a
Andriy Ivanyk <andriy@MacBook-Air.home>
2021-05-23 14:39:43 +0100

    Updates

---
+++ precache-manifest.8ee63a813c495301945b63fb9094e4f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ed50372df74a874a1a8298ea10fac2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abfedaf9bc9269f6e798",
+    "url": "./static/css/main.f4349526.chunk.css"
+  },
+  {
+    "revision": "828bb0aaf9acd9abc8c4",
+    "url": "./static/js/2.278a2e2b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.278a2e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abfedaf9bc9269f6e798",
+    "url": "./static/js/main.a1cd5c68.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.8ee63a813c495301945b63fb9094e4f1.js"
 );
 
 self.addEventListener('message', (event) => {
